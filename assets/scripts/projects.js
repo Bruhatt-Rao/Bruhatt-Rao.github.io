cbe5544ef213dfc1e2c6f000beb796d6ded06508
Bruhatt Rao <thebesttt2424@gmail.com>
2024-07-17 22:32:46 -0700

    updating projects

--- assets/scripts/projects.js
+++ assets/scripts/projects.js
@@ -1,18 +1,5 @@
 var parent;
-let projects = [
-    {
-        "name" : "Bhim Algorithm's", 
-        "desc" : "Sorting Algorithm visualizer built in Javascript using the HTML Canvas Library",
-        "link" : "https://bhim-rao.github.io/sorting/",
-        "src"  : "sorting.png"
-    },
-    {
-        "name" : "Conway's Game of Life", 
-        "desc" : "Conway's Game of life implemented in Rust using macroquad",
-        "link" : "https://github.com/Bhim-Rao/GameOfLife",
-        "src"  : "conway.png"
-    }
-];
+let projects = [{'name': 'Benchmark', 'desc': 'A python script to test how long another file takes to run', 'src': 'benchmark.png', 'link': 'https://github.com/Bhim-Rao/Benchmark'}, {'name': 'GameOfLife', 'desc': "A rust implementation of Conway's game of life using macroquad", 'src': 'gameoflife.png', 'link': 'https://github.com/Bhim-Rao/GameOfLife'}, {'name': 'GeneticAlgorithm', 'desc': 'An example of how a Genetic Algorithm works written in rust', 'src': 'geneticalgorithm.png', 'link': 'https://github.com/Bhim-Rao/GeneticAlgorithm'}, {'name': 'SandSim', 'desc': 'A sand simulation written in Rust', 'src': 'sandsim.png', 'link': 'https://github.com/Bhim-Rao/SandSim'}, {'name': 'Simple-Neural', 'desc': 'A python library to run a simple lightweight neural network', 'src': 'simple-neural.png', 'link': 'https://github.com/Bhim-Rao/Simple-Neural'}, {'name': 'Sorting', 'desc': 'an HTML Canvas Visualization of sorting algorithms', 'src': 'sorting.png', 'link': 'https://github.com/Bhim-Rao/sorting'}, {'name': 'windTunnel', 'desc': 'an HTML Canvas Visualization of a wind tunnel', 'src': 'windtunnel.png', 'link': 'https://github.com/Bhim-Rao/windTunnel'}];
 
 window.onload = function() {
     console.log("running");
@@ -31,7 +18,7 @@ class Project {
     constructor(d) {
         this.name = d["name"];
         this.desc = d["desc"];
-        this.src  = d["src"];
+        this.src = d["src"];
         this.link = d["link"];
     }
 
