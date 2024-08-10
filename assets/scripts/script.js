784917728e4abe8867a4452e9621620672cae976
Bruhatt Rao <bruhatt.rao@gmail.com>
2024-08-09 18:08:17 -0700

    Adding Dark Mode

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,1 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+	set();
+});
 
+function dark() {
+	var darkMode = localStorage.getItem("darkMode");
+	if (darkMode == "false") {
+		document.documentElement.style.setProperty('--main', '#121212');
+		document.documentElement.style.setProperty('--secondary', '#fffcf2');
+		localStorage.setItem("darkMode", true);
+	} else {
+		document.documentElement.style.setProperty('--secondary', '#121212');
+		document.documentElement.style.setProperty('--main', '#fffcf2');
+		localStorage.setItem("darkMode", false);
+	}
+}
+
+function set() {
+	var darkMode = localStorage.getItem("darkMode");
+	if (darkMode == "true") {
+		document.documentElement.style.setProperty('--main', '#121212');
+		document.documentElement.style.setProperty('--secondary', '#fffcf2');
+	} else {
+		document.documentElement.style.setProperty('--secondary', '#121212');
+		document.documentElement.style.setProperty('--main', '#fffcf2');
+	}
+}
